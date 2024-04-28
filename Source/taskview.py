8b5fd9545c3b1e749bae3643a681de366d5da804
G6H4 <proplopaido@gmail.com>
2024-04-28 16:10:55 +0200

    major: experimenting with processes

--- Source/taskview.py
+++ Source/taskview.py
@@ -1,3 +1,27 @@
-import os
-import sys
-import time
+import psutil
+
+class TaskView():
+    def __init__(self) -> None:
+        self.processes = []
+    
+    def getAllProcesses(self) -> list:
+        for process in psutil.process_iter(["pid", "name"]):
+            processName = process.info["name"]
+            if processName in self.processes:
+                continue
+            if ".exe" not in processName:
+                continue
+            self.processes.append(process.info)
+        return self.processes
+    
+    def findProcess(self, processName) -> dict:
+        self.processes = self.getAllProcesses()
+        
+        for process in self.processes:
+            if processName in process["name"]:
+                return process
+        return None
+
+print(TaskView().findProcess("brave.exe"))
+
+# There are no comments because it's still a work in progress
